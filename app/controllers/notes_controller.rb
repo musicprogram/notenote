b054c891654a32c2f85187ead1d2bc9e0aaeb3e6
juancho <juan-o-henao@hotmail.es>
2014-11-24 22:44:15 -0500

    add index show notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,7 @@
 class NotesController < ApplicationController
-
+	before_action :find_note, only: [:show, :edit, :update, :destroy]
 	def index
-		
+		@notes = Note.all.order("created_at DESC")
 	end
 
 	def show
@@ -15,6 +15,11 @@ class NotesController < ApplicationController
 	def create
 		@note = Note.new(note_params)
 
+		if @note.save
+			redirect_to @note
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -32,7 +37,7 @@ class NotesController < ApplicationController
 	private
 	
 	def find_note
-		
+		@note = Note.find(params[:id])
 	end
 
 	def note_params
